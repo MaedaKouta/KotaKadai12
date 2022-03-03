06c02a7a89ae333b68de3a484147735b88e36eaa
MaedaKouta <maedakoutaa@gmail.com>
2022-03-03 11:54:52 +0900

    提出用のコミット

--- KotaKadai12/ViewController.swift
+++ KotaKadai12/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var noneTaxMoneyTextField: UITextField!
+    @IBOutlet private weak var taxRateTextField: UITextField!
+    @IBOutlet private weak var inTaxMoneyLabel: UILabel!
+    private let taxRateString = "taxRateText"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        taxRateTextField.text = UserDefaults.standard.string(forKey: taxRateString)
     }
 
+    @IBAction private func didTapCalculateButton(_ sender: Any) {
+        if let noneTaxMoneyInt = Int(noneTaxMoneyTextField.text ?? ""), let taxRateInt = Int(taxRateTextField.text ?? "") {
+            let inTaxMoneyInt = noneTaxMoneyInt + ( noneTaxMoneyInt * taxRateInt / 100 )
+            inTaxMoneyLabel.text = String(inTaxMoneyInt)
+            UserDefaults.standard.set(taxRateTextField.text, forKey: taxRateString)
+        } else {
+            inTaxMoneyLabel.text = ""
+        }
+    }
 
 }
 
